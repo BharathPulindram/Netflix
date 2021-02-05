878753cceaa732702c808459efe98e94fcf3f28e
Pulindram <bharath.pulindram@acsicorp.com>
2021-02-05 20:07:03 +0530

    first commit

---
+++ src/Components/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Nav.css";
+function Nav() {
+  return (
+    <div className="nav">
+      <img
+        className="nav__logo"
+        src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
+        alt=""
+      />
+      <img
+        className="nav__avatar"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDh-Lf6cp-6GQjCqN1xwZOfQtkdbcTx4XA",
+  authDomain: "netflix-pulindram.firebaseapp.com",
+  projectId: "netflix-pulindram",
+  storageBucket: "netflix-pulindram.appspot.com",
+  messagingSenderId: "438649672813",
+  appId: "1:438649672813:web:202dd2df2e127138f2db64",
+};
