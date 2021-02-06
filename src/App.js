32db501523e9fde0d8807154b724a9d13bc0960d
Pulindram <bharath.pulindram@acsicorp.com>
2021-02-06 15:19:05 +0530

    Netflix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomeScreen from "./Components/HomeScreen";
-
+import LoginScreen from "./Screens/LoginScreen";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import ProfileScreen from "./Screens/ProfileScreen";
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout);
+      }
+    });
+    return unsubscribe;
+  }, []);
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+      </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyDh-Lf6cp-6GQjCqN1xwZOfQtkdbcTx4XA",
   authDomain: "netflix-pulindram.firebaseapp.com",
@@ -6,3 +7,7 @@ const firebaseConfig = {
   messagingSenderId: "438649672813",
   appId: "1:438649672813:web:202dd2df2e127138f2db64",
 };
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+export const auth = firebase.auth();
+export default db;
